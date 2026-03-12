2166cb78d9f2d77fb4c6df8ded7b55ce91ab8124
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-03-11 22:07:17 -0300

    feat: add method serach by id and word Co-authored-by: GoBrazill <vinicius.daraujos26@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const conselhoGerado = document.getElementById("conselho-gerado")
-const btn = document.getElementById("conselho")
-
-const buscarConselho = () => {
+const generatedAdvice = document.getElementById("advice-generated")
+const btn = document.getElementById("adviceButton")
+const inputId = document.getElementById("adviceId");
+const inputWord = document.getElementById("adviceWord");
+// pesquisar por um conselho aleatório
+const searchRandomAdvice = () => {
 
     fetch(`https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`)
 
@@ -9,13 +11,55 @@ const buscarConselho = () => {
         .then((data) => {
             console.log(data.slip.advice)
 
-            conselhoGerado.innerText = data.slip.advice
+            generatedAdvice.innerText = data.slip.advice
         })
         .catch((err) => {
-            conselhoGerado.innerText = "Erro ao encontrar o conselho!"
+            generatedAdvice.innerText = "Error to find the advice!"
         })
 
 }
 btn.addEventListener("click", () => {
-    buscarConselho()
+    searchRandomAdvice()
 })
+// pesquisar consleho por id
+const searchAdviceById = (id) => {
+  fetch(`https://api.adviceslip.com/advice/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      generatedAdvice.innerText = data.slip.advice;
+    })
+    .catch(() => {
+      generatedAdvice.innerText = "Nenhum conselho encontrado com esse ID!";
+    });
+};
+// Conselho por palavra
+const searchAdviceByWord = (word) => {
+  fetch(`https://api.adviceslip.com/advice/search/${word}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.slips && data.slips.length > 0) {
+        generatedAdvice.innerText = data.slips[0].advice;
+      } else {
+        generatedAdvice.innerText = "We don't have any advice with this word!";
+      }
+    })
+    .catch(() => {
+      generatedAdvice.innerText = "Error in search!";
+    });
+};
+
+// Eventos
+
+// Enter no input de ID
+inputId.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && inputId.value) {
+    searchAdviceById(inputId.value);
+  }
+});
+
+// Enter no input de palavra
+inputWord.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && inputWord.value) {
+    searchAdviceByWord(inputWord.value);
+  }
+});
